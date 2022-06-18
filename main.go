fc49d46646cf63ba350b81e1652c24fe5142037a
Semat <s.matveyev31@gmail.com>
2022-06-18 10:47:18 +0300

    Creat story/cart package

--- main.go
+++ main.go
@@ -4,12 +4,22 @@ import (
 	"fmt"
 	currencyFmt "packages/fmt"
 	"packages/store"
+	"packages/store/cart"
 )
 
 func main() {
 	product := store.NewProduct("Kayak", "Watersports", 279)
+	product1 := store.NewProduct("Padle", "Watersports", 200)
+
+	cart := cart.Cart{
+		CustomerName: "Alice",
+		Products:     []store.Product{*product, *product1},
+	}
 
 	fmt.Println("Name:", product.Name)
 	fmt.Println("Category:", product.Category)
 	fmt.Println("Price:", currencyFmt.ToCurrency(product.Price()))
+
+	fmt.Println("Name:", cart.CustomerName)
+	fmt.Println("Total:", currencyFmt.ToCurrency(cart.GetTotal()))
 }
