50318054397757dfa9acfb6d51ac2d0c92509e4b
Semat <s.matveyev31@gmail.com>
2022-06-20 07:59:27 +0300

    Add categoryMaxPrice map

--- store/tax.go
+++ store/tax.go
@@ -3,13 +3,20 @@ package store
 const defaultTaxRate float64 = 0.2
 const minThreshold = 10
 
+// categoryMaxPrice ...
+var categoryMaxPrice = map[string]float64{
+	"Watersports": 250 + (250 * defaultTaxRate),
+	"Soccer":      150 + (150 * defaultTaxRate),
+	"Chess":       50 + (50 * defaultTaxRate),
+}
+
 // taxRate ...
 type taxRate struct {
 	rate      float64
 	threshold float64
 }
 
-// newTaxRate ...
+// newTaxRate: taxRate structure construcctor.
 func newTaxRate(rate, threshold float64) *taxRate {
 	if rate == 0 {
 		rate = defaultTaxRate
@@ -21,9 +28,15 @@ func newTaxRate(rate, threshold float64) *taxRate {
 }
 
 // calcTax ...
-func (taxRate *taxRate) calcTax(product *Product) float64 {
+func (taxRate *taxRate) calcTax(product *Product) (price float64) {
 	if product.ptice > taxRate.threshold {
-		return product.ptice + (product.ptice * taxRate.rate)
+		price = product.ptice + (product.ptice * taxRate.rate)
+	} else {
+		price = product.ptice
 	}
-	return product.ptice
+
+	if max, ok := categoryMaxPrice[product.Category]; ok && price > max {
+		price = max
+	}
+	return
 }
