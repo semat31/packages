d479e6c1508645be376a48b57d482b0783392063
Sergey Matveyev <59117579+semat31@users.noreply.github.com>
2022-12-25 16:16:53 +0000

    creat WeekRange function

--- main.go
+++ main.go
@@ -1,22 +1,70 @@
 package main
 
 import (
-	_ "packages/data"
-	currencyFmt "packages/fmt"
-	"packages/store"
-	"packages/store/cart"
-
-	"github.com/fatih/color"
+	"fmt"
+	"strconv"
+	"time"
 )
 
-func main() {
-	product := store.NewProduct("Kayak", "Watersports", 279)
+type CWeek struct {
+	WeekISO string
 
-	cart := cart.Cart{
-		CustomerName: "Alice",
-		Products:     []store.Product{*product},
+	//WeekStart time.Time
+	//WeekEnd time.Time
+}
+
+func CWeekNew(t time.Time) (cw CWeek) {
+	year, week := t.ISOWeek()
+	cw.WeekISO = strconv.Itoa(year) + "W" + strconv.Itoa(week)
+
+	return
+}
+
+func WeekStart(year, week int) time.Time {
+	// Start from the middle of the year:
+	t := time.Date(year, 7, 1, 0, 0, 0, 0, time.UTC)
+
+	// Roll back to Monday:
+	if wd := t.Weekday(); wd == time.Sunday {
+		t = t.AddDate(0, 0, -6)
+	} else {
+		t = t.AddDate(0, 0, -int(wd)+1)
 	}
 
-	color.Green("Name: " + cart.CustomerName)
-	color.Cyan("Total: " + currencyFmt.ToCurrency(cart.GetTotal()))
+	// Difference in weeks:
+	_, w := t.ISOWeek()
+	t = t.AddDate(0, 0, (week-w)*7)
+
+	return t
+}
+
+func WeekRange(year, week int) (start, end time.Time) {
+	start = WeekStart(year, week)
+	end = start.AddDate(0, 0, 6)
+	return
+}
+
+func main() {
+	var t_day = "25.12.2022"
+
+	var date_layout = "02.01.2006"
+
+	t, err := time.Parse(date_layout, t_day)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Извлечение единиц из: %v\n", t)
+	dOfMonth := t.Day()
+	weekDay := t.Weekday()
+	month := t.Month()
+	fmt.Printf("%d-й день из %v это %v\n", dOfMonth, month, weekDay)
+
+	cWeek := CWeekNew(t)
+	fmt.Println(cWeek.WeekISO)
+
+	fmt.Println(WeekRange(2022, 51))
+	fmt.Println(WeekRange(2018, 2))
+	fmt.Println(WeekRange(2019, 1))
+	fmt.Println(WeekRange(2019, 2))
 }
